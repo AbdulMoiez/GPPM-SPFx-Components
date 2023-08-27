b69a76ca50fb29cb875dd984a29f89f17fc1ff20
Usama Ali <info.usamaali@gmail.com>
2023-08-27 10:23:51 +0500

    SPFX Application Demo Setup

---
+++ Spfx Application/src/webparts/essWaitingForYourInput/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IEssWaitingForYourInputWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'EssWaitingForYourInputWebPartStrings' {
+  const strings: IEssWaitingForYourInputWebPartStrings;
+  export = strings;
+}
